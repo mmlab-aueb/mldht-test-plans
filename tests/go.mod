44c2d6117c9907a36837ed1de3dce3aa852a9abe
Nikos Fotiou <nikosft@gmail.com>
2021-04-20 02:45:11 +0300

    libp2p added, go sucks

--- tests/go.mod
+++ tests/go.mod
@@ -2,4 +2,9 @@ module github.com/mmlab-aueb/mldht-test-plans/tests
 
 go 1.16
 
-require github.com/testground/sdk-go v0.2.7
+require (
+	github.com/ipfs/go-log/v2 v2.1.3
+	github.com/libp2p/go-libp2p v0.13.0
+	github.com/multiformats/go-multiaddr-net v0.2.0
+	github.com/testground/sdk-go v0.2.7
+)
--- tests/store_lookup.go
+++ tests/store_lookup.go
@@ -3,10 +3,15 @@ package main
 import (
 	"context"
 	"net"
+	"fmt"
 
 	"github.com/testground/sdk-go/runtime"
 	"github.com/testground/sdk-go/sync"
 	"github.com/testground/sdk-go/network"
+	"github.com/libp2p/go-libp2p"
+	manet "github.com/multiformats/go-multiaddr-net"
+
+	"github.com/ipfs/go-log/v2"
 )
 
 type NodeInfo struct
@@ -15,6 +20,26 @@ type NodeInfo struct
 }
 
 var node_info_topic = sync.NewTopic("nodeinfo", &NodeInfo{})
+
+func getSubnetAddr(runenv *runtime.RunEnv) (*net.TCPAddr, error) {
+	log.SetAllLoggers(log.LevelWarn)
+	subnet := runenv.TestSubnet
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, addr := range addrs {
+		if ip, ok := addr.(*net.IPNet); ok {
+			if subnet.Contains(ip.IP) {
+				tcpAddr := &net.TCPAddr{IP: ip.IP}
+				return tcpAddr, nil
+			}
+		} else {
+			panic(fmt.Sprintf("%T", addr))
+		}
+	}
+	return nil, fmt.Errorf("no network interface found. Addrs: %v", addrs)
+}
 
 func StoreLookup(runenv *runtime.RunEnv) error {
 	ctx := context.Background()
@@ -43,6 +68,15 @@ func StoreLookup(runenv *runtime.RunEnv) error {
 		entry := <-node_info_channel
 		runenv.RecordMessage("Received from channel %s", entry.IP.String())
 	}
+	tcpAddr, err := getSubnetAddr(runenv)
+	addr, err := manet.FromNetAddr(tcpAddr)
+	host, err := libp2p.New(ctx,
+		libp2p.ListenAddrs(addr),
+	)
+	if err != nil {
+		panic(err)
+	}
+	runenv.RecordMessage("Host created. We are:", host.ID())
 
 	runenv.RecordMessage("Ending experiment")
 	return nil
